8305d0a056bb43e14cea9a28981c8c3724ae5dac
abdihafidgahayr2-glitch <abdihafidgahayr2@gmail.com>
2026-03-25 14:45:54 +0000

    Created shared files

---
+++ src/main/java/com/library/shared/RequestType.java
@@ -0,0 +1,14 @@
+package com.library.shared;
+
+/**
+ * Enumerates the supported JSON protocol request types for the Library Management System.
+ */
+public enum RequestType
+{
+    GET_ALL,
+    GET_BY_ID,
+    INSERT,
+    UPDATE,
+    DELETE,
+    DISCONNECT
+}
