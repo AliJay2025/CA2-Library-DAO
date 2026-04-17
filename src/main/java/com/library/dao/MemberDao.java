0f40057f748d24306c2d3b9b9ae02dd608e2ec9c
Ali jabriil <D00283862@student.dkit.ie>
2026-04-17 17:28:37 +0100

    F17: Extend MemberDao interface for BLOB operations

--- src/main/java/com/library/dao/MemberDao.java
+++ src/main/java/com/library/dao/MemberDao.java
@@ -3,14 +3,19 @@ package com.library.dao;
 import com.library.domain.Member;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 
 public interface MemberDao {
+    // Existing methods
     int insert(String name, String address, String phone) throws Exception;
     Optional<Member> findById(int id) throws Exception;
     List<Member> findAll() throws Exception;
-    Member update(int id, Member member) throws Exception;
+    boolean update(int id, Member member) throws Exception;
     boolean deleteById(int id) throws Exception;
 
-    List<Member> findByFilter(Predicate<Member> filter) throws Exception;
+    // F17: New method for inserting member with image
+    int insertMemberWithImage(Member member) throws Exception;
+
+    // F20: Metadata-only queries (no BLOB)
+    Optional<Member> findMetadataById(int id) throws Exception;
+    List<Member> findAllMetadataOnly() throws Exception;
 }
