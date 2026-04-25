d69a6fc55ec5a870b50b4587f52f22f7f3cb1d7c
Ali jabriil <D00283862@student.dkit.ie>
2026-04-25 14:39:25 +0100

    Complete update of the RequestType....

--- src/main/java/com/library/shared/RequestType.java
+++ src/main/java/com/library/shared/RequestType.java
@@ -1,5 +1,7 @@
 package com.library.shared;
 
+// All possible request types the client can send to the server
+
 public enum RequestType
 {
     GET_ALL,
@@ -7,6 +9,8 @@ public enum RequestType
     INSERT,
     UPDATE,
     DELETE,
-    UPLOAD,      // F18: Binary file upload
-    DISCONNECT
+    UPLOAD,       // Upload a file/image for a member - F18
+    DOWNLOAD,     // Download a file/image from a member - F19
+    METADATA,     // Get file metadata without downloading the file - F20
+    DISCONNECT    // Tell server client is closing connection - F21
 }
